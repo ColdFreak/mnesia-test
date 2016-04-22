6696dbeae8816cb7b2551adef1d1ea26d559b2d8
Wang Zhijun <so@idcf.jp>
2016-04-22 18:22:42 +0900

    add functions

--- lib/read_test.ex
+++ lib/read_test.ex
@@ -1,6 +1,25 @@
 defmodule ReadTest do
   use Database
   use Amnesia
+  def print_all(table_name) do
+    Amnesia.transaction do
+      iterator = fn(rec, _newAcc) ->
+        IO.puts "#{inspect rec}"
+      end
+      Amnesia.Table.foldl(table_name, [], iterator)
+      # :ets.tab2list(User)
+    end
+  end
+
+  # `User`テーブルのなかの`name`を出力
+  def find_user_by_email(user_email) do
+    Amnesia.transaction do
+      selection = User.where email == user_email,
+        select: name
+      selection |> Amnesia.Selection.values |> Enum.each(&IO.puts(&1))
+    end
+  end
+
   def read_message(user_id) do
     Amnesia.transaction do
       john = User.read(user_id)
